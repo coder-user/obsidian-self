be4bce27299fce87e6f6399c7ff801f2aea21af2
coolliuzw <1413883407@qq.com>
2023-04-25 21:46:33 +0800

    markdown modify

--- main.ts
+++ main.ts
@@ -14,124 +14,52 @@ export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+			id: 'coolliuzw-markdown-modify',
+			name: 'Coolliuzw Markdown Modify',
+			editorCallback: markdownModify
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
 	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+function markdownModify(editor: Editor, view: MarkdownView) {
+	// 获取当前编辑器的全部文本
+	const fullText = editor.getValue();
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	// 将 "**修订记录**" 替换为 "## 修订记录"
+	// 将 "**名词解释**" 替换为 "## 名词解释"
+	let newDataStr = fullText.replace(/\*\*修订记录\*\*/g, '## 修订记录');
+	newDataStr = newDataStr.replace(/\*\*名词解释\*\*/g, '## 名词解释');
+  
+	// 如果文件中没有 [[_TOC_]]，则在 ## 修订记录 之前添加该标记
+	if (!newDataStr.includes('[[_TOC_]]')) {
+	  newDataStr = newDataStr.replace('## 修订记录', '[[_TOC_]]\n\n## 修订记录');
 	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+  
+	// 将图片链接转换为相对路径
+	const imgRe = /!\[\[(.*)\]\]/g;
+	newDataStr = newDataStr.replace(imgRe, (match, imageName) => {
+	  imageName = imageName.replace(/ /g, '%20');
+	  return `![${imageName}](resource/img/${imageName})`;
+	});
+  
+	 // 将 "## 数字" 标题修改为 "## 大写字母、"
+	const re = /^(##\s+)(\d+)\s+(.*)$/gm;
+	newDataStr = newDataStr.replace(re, (match, prefix, number, title) => {
+		const letter = numToChinese(number);
+		return `${prefix}${letter}、 ${title}`;
+	});
+  
+	// 将处理后的文本写回编辑器
+	editor.setValue(newDataStr);
 }
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+// 将数字转换为大写字母
+function numToChinese(num: number) {
+	const words = ['一', '二', '三', '四', '五', '六', '七', '八', '九'];
+	return words[num - 1];
 }
